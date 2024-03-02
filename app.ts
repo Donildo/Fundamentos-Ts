4d05b46c095d593bbd73dfa3d591b908c41176ad
Donildo <donildobrito307@gmail.com>
2024-03-02 16:37:56 -0400

    Dio Bank

--- app.ts
+++ app.ts
@@ -1,40 +1,9 @@
-// Dio Banking
+import { CompanyAccount } from './class/CompanyAccount'
+import { PeopleAccount } from './class/PeopleAccount'
 
-//name, acountNumber
-//depositar, sacar
-
-abstract class Account {
-    name: string
-    accountNumber: Number
-    balance: number = 0
-
-    constructor(name: string, accountNumber: number) {
-        this.name = name
-        this.accountNumber = accountNumber
-    }
-
-    deposit = () => {
-        console.log('Voce depositou')
-    }
-
-    withdraw = () => {
-        console.log('Voce sacou')
-    }
-
-    getBalance =() => {
-        console.log(this.balance)
-    }
-}
-
-class PeopleAcount extends Account {
-    doc_id: number
-
-    constructor(doc_id: number, name: string, accountNumber: number){
-        super(name, accountNumber)
-        this.doc_id = doc_id
-    }
-
-}
-
-const peopleAccount: PeopleAcount = new PeopleAcount (1, 'Donildo', 10)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Donildo', 10)
 console.log(peopleAccount)
+peopleAccount.deposit()
+const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+companyAccount.deposit()
+console.log(companyAccount)
